2538cd2a18ee0e8d1c32c8c1bf7f013c82ba5008
imabari <imabari@gmail.com>
2024-06-29 21:36:34 +0900

    toubani

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import pathlib
 
 import pandas as pd
 
-df_toubai = pd.read_csv("toubani.csv")
+df_toubani = pd.read_csv("toubani.csv")
 df_list = pd.read_csv("hosp_list.csv")
 
 # 当番医と病院情報を結合する
-df_hosp = pd.merge(df_toubai, df_list, on="name", how="left")
+df_hosp = pd.merge(df_toubani, df_list, on="name", how="left")
 
 df_hosp.to_csv("latest.csv", index=False)
 
